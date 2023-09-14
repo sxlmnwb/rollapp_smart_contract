6e597c224f90daf80faff2cc50271d788d63e6a8
Salman Wahib <sxlmnwb.dev@gmail.com>
2023-09-14 03:15:38 +0000

    ERC-721: initial add base tamplate on rollapp
    Signed-off-by: Salman Wahib <hello@sxlmnwb.xyz>

---
+++ NFT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/utils/Strings.sol";
+
+contract NFT is ERC721 {
+    uint256 public currentTokenId;
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
+
+    function mintTo(address recipient) public payable returns (uint256) {
+        uint256 newItemId = ++currentTokenId;
+        _safeMint(recipient, newItemId);
+        return newItemId;
+    }
+
+    function tokenURI(uint256 id) public view virtual override returns (string memory) {
+        return Strings.toString(id);
+    }
+}
